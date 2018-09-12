5c08d58c884042d4928ada6ee55e142c33e4409f
Rob Kooper <kooper@illinois.edu>
2018-09-12 14:26:11 -0500

    implement calls/season calls

--- bety_brapi/__main__.py
+++ bety_brapi/__main__.py
@@ -12,12 +12,6 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'BrAPI'})
-    app.run(port=8080)
-
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
-
-    app.app.logger.addHandler(handler)
     app.app.logger.setLevel(logging.DEBUG)
 
     app.run(port=8080)
---
+++ bety_brapi/controllers_impl/CallsController_impl.py
@@ -0,0 +1,52 @@
+from bety_brapi import helper
+
+VERSIONS_ALL = ["1.0", "1.1", "1.2"]
+VERSIONS_LATEST = ["1.2"]
+
+
+def calls_get(datatype, pageSize, page):
+    """
+    Return a list of all calls that are implemented
+    """
+
+    # create the full list of all calls implemented.
+    data = [
+        calls_get_helper('calls'),
+        calls_get_helper('phenotypes-search'),
+        calls_get_helper('seasons')
+    ]
+
+    # filter on datatype
+    if datatype:
+        data = [d for d in data if datatype in d['datatypes']]
+
+    # total number of rows
+    count = len(data)
+
+    # add a limit
+    if not pageSize:
+        pageSize = len(data)
+    if not page:
+        page = 0
+    data = data[page * pageSize:(page+1) * pageSize]
+
+    # return the resulting data
+    return helper.create_result(data, count, pageSize, page)
+
+
+def calls_get_helper(api_call, datatypes=None, methods=None, versions=None):
+    """
+    Helper function to return a brapi call information.
+    """
+    if not datatypes:
+        datatypes = ["json"]
+    if not methods:
+        methods = ['GET']
+    if not versions:
+        versions = VERSIONS_ALL
+    return {
+            "call": api_call,
+            "datatypes": datatypes,
+            "methods": methods,
+            "versions": versions
+        }
