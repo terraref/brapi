512d828ddb8443fa34a965803d7353154a04f919
Rob Kooper <kooper@illinois.edu>
2019-05-02 11:29:41 +0200

    implemented programs endpoint

--- api/calls.py
+++ api/calls.py
@@ -18,6 +18,7 @@ def search(datatype=None, dataType=None, pageSize=None, page=None):
         calls_get_helper('locations'),
         calls_get_helper('locations/{locationDbId}'),
         calls_get_helper('phenotypes', methods=['POST']),
+        calls_get_helper('programs'),
         calls_get_helper('seasons')
     ]
 
