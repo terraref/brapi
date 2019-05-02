227499495cf3f00cd656562f46a7e9295d6248dd
Rob Kooper <kooper@illinois.edu>
2019-05-02 15:25:35 +0200

    Remove older version of codebase

--- swagger-gen.sh
+++
@@ -1,8 +0,0 @@
-# Regenerate based on the Swagger spec
-
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate \
-    -i https://app.swaggerhub.com/apiproxy/schema/file/PlantBreedingAPI/BrAPI/1.3/swagger.json \
-    -l python-flask \
-    -c /local/config.json \
-    -t /local/templates/flaskConnexion \
-	-o /local 
