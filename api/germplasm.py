be8fadc176e314e5cf6a6f7d189e5d22ac89f280
chris <schnaufer@email.arizona.edu>
2019-05-02 02:06:02 -0700

    More fixup

--- api/germplasm.py
+++ api/germplasm.py
@@ -27,23 +27,24 @@ def search(germplasmPUI=None, germplasmDbId=None, germplasmName=None, commonCrop
         page = 0
     data = data[page * pageSize:(page+1) * pageSize]
 
-    nd = _convert_to_string(_flatten_data(data))
+    nd = _ensure_arrays(_convert_to_string(_flatten_data(data)))
 
     # return the resulting data
     return helper.create_result({"data": nd}, count, pageSize, page)
 
-no_flatten_names = ["donors", "synonyms"]
+no_flatten_names = []
+ensure_array_names = ["donors", "synonyms"]
 int_to_str_names = ["germplasmDbId", "typeOfGermplasmStorageCode"]
-str_to_int_names = ["typeOfGermplasmStorageCode"]
+str_to_int_names = ["biologicalStatusOfAccessionCode"]
 
 def _flatten_data(data):
     if isinstance(data, list):
         if len(data) == 1:
-            return data[0]
+            return _flatten_data(data[0])
         else:
             return [_flatten_data(x) for x in data]
     if isinstance(data, dict):
-        return {k: _flatten_data(v) if not k in no_flatten_names else v for k, v in data.items()}
+        return {k: _flatten_data(v) for k, v in data.items()}
     return data
 
 def _convert_to_string(data):
@@ -54,6 +55,17 @@ def _convert_to_string(data):
               _convert_to_string(v) if not k in int_to_str_names else \
               str(v) if not k in str_to_int_names else \
               _make_int(v) for k, v in data.items()}
+
+    if isinstance(data, int):
+        return str(data)
+
+    return data
+
+def _ensure_arrays(data):
+    if isinstance(data, list):
+        return [_ensure_arrays(x) for x in data]
+    if isinstance(data, dict):
+        return {k: v if not k in ensure_array_names or v is None or isinstance(v, list) else [v] for k, v in data.items()}
     return data
 
 def _make_int(data):
